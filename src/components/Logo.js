69e252284c911b5315f62c01432c943c655a61fd
fatemafreezzy <fatemafreezzy@gmail.com>
2022-06-29 23:33:40 +0530

    sosupdate

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Image, StyleSheet } from 'react-native'
 
 export default function Logo({style}) {
-  return <Image source={require('../assets/logo.png')} style={[styles.image,style]} />
+  return <Image source={require('../assets/logo.jpg')} style={[styles.image,style]} />
 }
 
 const styles = StyleSheet.create({
